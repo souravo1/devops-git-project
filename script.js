2459777dc5ac97c6784b5f17503f1a5087baa1a5
souravo1 <sk4668464@gmail.com>
2025-09-02 16:40:54 +0530

    Added website for hosting.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth scroll for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
